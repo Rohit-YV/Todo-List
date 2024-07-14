c805d76fe51059e26506d013c42c92b15008669e
Ranjan-Yadav <111560232+Rohit-YV@users.noreply.github.com>
2024-07-14 20:07:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,38 @@
 import express from "express";
 import bodyParser from 'body-parser';
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+let items = []; 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); 
 
-app.get("/",(req,res)=>{
-var today = new Date();
-var currentDay = today.getDay();
-var day ="";
-switch(currentDay){
-case 0:
-    day = "Sunday";
-    break;
-case 1:
-    day = "Monday";
-    break;
-case 2:
-    day = "Tuesday";
-    break;
-case 3:
-    day = "Wednesday";
-    break;
-case 4:
-    day = "Thursday";
-    break;
-case 5:
-    day = "Friday";
-    break;
-case 6:
-    day = "Saturday";
-    break;
-default:
-    console.log("Error:current day is equal to :"+ currentDay);
-}
+app.get("/", (req, res) => {
+    const today = new Date();
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    const day = today.toLocaleDateString("en-US", options);
 
-res.render("list",{
-    kindofday: day
+    res.render("list", {
+        kindofday: day,
+        newlistitems: items 
+    });
 });
-});
-app.listen(3000,()=>{
-    console.log("server is listening on port 3000");
 
+app.post("/", (req, res) => {
+    const item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
 });
